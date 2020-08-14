a63c8f25ed340388e56bdac650073ca43090cd5f
Azucena Herrera <azucenaphx@gmail.com>
2020-08-14 12:56:55 -0700

    removed req and commented some new properties on user model

--- models/user.js
+++ models/user.js
@@ -2,19 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  // authO: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   phone: { type: Number, required: true },
   preferences: {
     priceRange: {
-           from: {type:  Number, required: true},
+           from: {type:  Number},
            to: {type: Number, require: true }
       },
-    bedrooms: { type: Number, required: true }, 
-    bathrooms: { type: Number, required: true }, 
-    propertyType: { type: String, required: true },
+    bedrooms: { type: Number }, 
+    bathrooms: { type: Number }, 
+    propertyType: { type: String },
     creationDate: { type: Date, default: Date.now },
     updateDate: { type: Date, default: Date.now }
     }, 
